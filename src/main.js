2f6e6efa88e8a9ce83f3d32800188d7fb30a7161
yaisa03 <yaisamichellea@gmail.com>
2022-02-22 21:07:42 -0500

    html, imagenes y js historia 1

--- src/main.js
+++ src/main.js
@@ -1,7 +1,42 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
-console.log("HI GIRLS")
+const buttonAccess = document.getElementById("buttonAccess");
+buttonAccess.addEventListener("click", showPage);
+
+function showPage(){
+    document.getElementById("contentPageOne").style.display="none";
+    document.getElementById("greetingPageOne").style.display="block";
+    let inputName = document.getElementById("userInput").value;
+    document.getElementById("userName").innerText = inputName ;
+    setTimeout(showPage2, 4000);
+}
+function showPage2(){
+    document.getElementById("pageOne").style.display="none";
+    document.getElementById("pageTwo").style.display="block";
+}
+
+/*function obtenerHtmlListado(){
+
+  const filmsDiv = document.getElementById('listadoPeliculas')
+  let conjuntoFilms=''
+
+  for (let i=0;i< peliculasAMostrar.length ; i++){
+    let peliculaDiv = '<div>'
+
+    peliculaDiv = peliculaDiv + '<img src="'+ peliculasAMostrar[i].poster +'">'
+    peliculaDiv = peliculaDiv + '<h2>'
+    peliculaDiv = peliculaDiv + data.films[i].title;
+    peliculaDiv = peliculaDiv + '</h2>'
+    peliculaDiv = peliculaDiv + '</div>'
+
+    conjuntoFilms = peliculaDiv + conjuntoFilms
+  }
+  filmsDiv.innerHTML = conjuntoFilms;
+
+console.log(conjuntoFilms);
+}
+
+console.log( data.films);*/
