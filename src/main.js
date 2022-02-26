3f02a2ff71bf328349d4475ddc28530289d4e8cf
yaisa03 <yaisamichellea@gmail.com>
2022-02-25 23:22:01 -0500

    primera vista de las peliculas

--- src/main.js
+++ src/main.js
@@ -1,49 +1,37 @@
-//import { filterData /*sortData, computeStats*/ } from './data.js';
-//import films from './data/ghibli/ghibli.js';
+//import { filterData, sortData, computeStats } from './data.js';
+import data from './data/ghibli/ghibli.js';
+
+const arrayFilms = data.films;
 
 const buttonAccess = document.getElementById("buttonAccess");
 buttonAccess.addEventListener("click", showPage);
 
-function showPage(){
-    document.getElementById("contentPageOne").style.display="none";
-    document.getElementById("greetingPageOne").style.display="block";
-    let inputName = document.getElementById("userInput").value;
-    document.getElementById("userName").innerText = inputName ;
-    setTimeout(showPage2, 2000);
+function showPage() {
+  document.getElementById("contentPageOne").style.display = "none";
+  document.getElementById("greetingPageOne").style.display = "block";
+  let inputName = document.getElementById("userInput").value;
+  document.getElementById("userName").innerText = inputName;
+  setTimeout(showPage2, 3000);
 }
-function showPage2(){
-    document.getElementById("pageOne").style.display="none";
-    document.getElementById("pageTwo").style.display="block";
-   
-  
+function showPage2() {
+  document.getElementById("pageOne").style.display = "none";
+  document.getElementById("pageTwo").style.display = "block";
+  showFilms();
 }
- 
 
+function showFilms(){
+  const filmsDiv = document.getElementById("contentPageTwo");
+  let allFilms='';
+  for (let i=0;i< arrayFilms.length ; i++){
+    let filmDiv = '<div>'
 
+    filmDiv = filmDiv + '<img src="'+ arrayFilms[i].poster +'">'
+    filmDiv = filmDiv + '<h2>'
+    filmDiv = filmDiv + arrayFilms[i].title;
+    filmDiv = filmDiv + '</h2>'
+    filmDiv = filmDiv + '</div>'
 
-
-
-
-//AVANCE 
-/*function obtenerHtmlListado(){
-
-  const filmsDiv = document.getElementById('listadoPeliculas')
-  let conjuntoFilms=''
-
-  for (let i=0;i< peliculasAMostrar.length ; i++){
-    let peliculaDiv = '<div>'
-
-    peliculaDiv = peliculaDiv + '<img src="'+ peliculasAMostrar[i].poster +'">'
-    peliculaDiv = peliculaDiv + '<h2>'
-    peliculaDiv = peliculaDiv + data.films[i].title;
-    peliculaDiv = peliculaDiv + '</h2>'
-    peliculaDiv = peliculaDiv + '</div>'
-
-    conjuntoFilms = peliculaDiv + conjuntoFilms
+    allFilms = filmDiv + allFilms
   }
-  filmsDiv.innerHTML = conjuntoFilms;
-
-console.log(conjuntoFilms);
+  filmsDiv.innerHTML = allFilms;
 }
-
-console.log( data.films);*/
