4c49c02db2fadd42cee702a679671ff4ed52f189
yaisa03 <yaisamichellea@gmail.com>
2022-03-01 21:52:47 -0500

    icono flecha

--- src/main.js
+++ src/main.js
@@ -17,34 +17,39 @@ function showPage2() {
   document.getElementById("pageOne").style.display = "none";
   document.getElementById("pageTwo").style.display = "block";
   showFilms(arrayFilms);
+  /*mostrar historia 3*/
+  const filmsDisplay = document.querySelectorAll(`div[id^="film"]`);
+  showPage3(filmsDisplay, arrayFilms);
 }
 
-function showFilms(dataToPrint){
-  const filmsDiv = document.getElementById("contentPageTwo");
-  let allFilms='';
 
-  for (let i=0;i< dataToPrint.length ; i++){
-    let filmDiv = '<div>'
-    filmDiv = filmDiv + '<img src="'+ readPropertyFromFilm(dataToPrint[i], "poster") + '">';
+function showFilms(dataToPrint) {
+  const filmsDiv = document.getElementById("contentPageTwo");
+  let allFilms = '';
+
+  for (let i = 0; i < dataToPrint.length; i++) {
+    let filmDiv = `<div id="film${[i]}">`
+    filmDiv = filmDiv + '<img src="' + readPropertyFromFilm(dataToPrint[i], "poster") + '">';
     filmDiv = filmDiv + '<h2>'
-    filmDiv = filmDiv + readPropertyFromFilm(dataToPrint[i], "title") +'<br>';
+    filmDiv = filmDiv + readPropertyFromFilm(dataToPrint[i], "title") + '<br>';
     filmDiv = filmDiv + '</h2>'
     filmDiv = filmDiv + '<h4>'
-    filmDiv = filmDiv + '<p>Director: '+readPropertyFromFilm(dataToPrint[i], "director") +'</p>';
-    filmDiv = filmDiv + '<p>Productor: '+readPropertyFromFilm(dataToPrint[i], "producer")+'</p>';
-    filmDiv = filmDiv + readPropertyFromFilm(dataToPrint[i], "release_date") +'<br>';
+    filmDiv = filmDiv + '<p>Director: ' + readPropertyFromFilm(dataToPrint[i], "director") + '</p>';
+    filmDiv = filmDiv + '<p>Productor: ' + readPropertyFromFilm(dataToPrint[i], "producer") + '</p>';
+    filmDiv = filmDiv + readPropertyFromFilm(dataToPrint[i], "release_date") + '<br>';
     filmDiv = filmDiv + '</h4>'
     filmDiv = filmDiv + '</div>'
     allFilms = filmDiv + allFilms
   }
   filmsDiv.innerHTML = allFilms;
 }
+
 //manejo del DOM
-function  readPropertyFromFilm(data,property){
-  return  data[property]
+function readPropertyFromFilm(data, property) {
+  return data[property]
 }
 
-function filterDataByMovieTitleAsc(){
+function filterDataByMovieTitleAsc() {
   let orderData = sortData(data.films, "title", ORDER_ASCENDENTE);
 
   //limpiar el div contentPageTwo
@@ -56,28 +61,28 @@ function filterDataByMovieTitleAsc(){
 }
 
 let buttonFilterByMovieTitleAsc = document.getElementById("filterDataByMovieTitleAsc");
-buttonFilterByMovieTitleAsc.addEventListener("click",filterDataByMovieTitleAsc);
+buttonFilterByMovieTitleAsc.addEventListener("click", filterDataByMovieTitleAsc);
 
-function filterDataByMovieTitleDesc(){
+function filterDataByMovieTitleDesc() {
   let orderData = sortData(data.films, "title", ORDER_DESCENDENTE);
   const filmsDiv = document.getElementById("contentPageTwo");
   filmsDiv.innerHTML = "";
   showFilms(orderData);
 }
 let buttonFilterByMovieTitleDesc = document.getElementById("filterDataByMovieTitleDesc");
-buttonFilterByMovieTitleDesc.addEventListener("click",filterDataByMovieTitleDesc);
+buttonFilterByMovieTitleDesc.addEventListener("click", filterDataByMovieTitleDesc);
 
 
 //EVENTOS PARA DIRECTORES
 //----------evento ascendente-----------
 let buttonFilterByMovieDirectorAsc = document.getElementById("filterDataByMovieDirectorAsc");
-buttonFilterByMovieDirectorAsc.addEventListener("click",filterDataByMovieDirectorAsc);
+buttonFilterByMovieDirectorAsc.addEventListener("click", filterDataByMovieDirectorAsc);
 
-function filterDataByMovieDirectorAsc(){
+function filterDataByMovieDirectorAsc() {
   let orderData = sortData(data.films, "director", ORDER_ASCENDENTE);
   const onlyDirectors = [];
   orderData.forEach(p => {
-    if(onlyDirectors.findIndex(pd => pd.director === p.director) === -1) {
+    if (onlyDirectors.findIndex(pd => pd.director === p.director) === -1) {
       // No existe; al detectar que no existe el mismo nombre, "la copiamos"
       onlyDirectors.push(p);
     }
@@ -87,17 +92,17 @@ function filterDataByMovieDirectorAsc(){
   filmsDiv.innerHTML = "";
   //mostrar directores
   showFilmsDirector(onlyDirectors);
- }
+}
 
- //----------evento descendente-----------
- let buttonFilterByMovieDirectorDesc = document.getElementById("filterDataByMovieDirectorDesc");
- buttonFilterByMovieDirectorDesc.addEventListener("click",filterDataByMovieDirectorDesc);
+//----------evento descendente-----------
+let buttonFilterByMovieDirectorDesc = document.getElementById("filterDataByMovieDirectorDesc");
+buttonFilterByMovieDirectorDesc.addEventListener("click", filterDataByMovieDirectorDesc);
 
- function filterDataByMovieDirectorDesc(){
+function filterDataByMovieDirectorDesc() {
   let orderData = sortData(data.films, "director", ORDER_DESCENDENTE);
   const onlyDirectors = [];
   orderData.forEach(p => {
-    if(onlyDirectors.findIndex(pd => pd.director === p.director) === -1) {
+    if (onlyDirectors.findIndex(pd => pd.director === p.director) === -1) {
       // No existe; al detectar que no existe el mismo nombre, "la copiamos"
       onlyDirectors.push(p);
     }
@@ -107,25 +112,25 @@ function filterDataByMovieDirectorAsc(){
   filmsDiv.innerHTML = "";
   //mostrar directores
   showFilmsDirector(onlyDirectors);
- }
+}
 
 //MOSTRAR DIRECTORES
-function showFilmsDirector(dataToPrint){
+function showFilmsDirector(dataToPrint) {
   const filmsDiv = document.getElementById("contentPageTwo");
-  let allFilms='';
+  let allFilms = '';
 
-  for (let i=0;i< dataToPrint.length ; i++){
-    let filmDiv = '<article>'
-    filmDiv = filmDiv + '<img src="'+ readPropertyFromFilm(dataToPrint[i], "posterDirector") + '">';
+  for (let i = 0; i < dataToPrint.length; i++) {
+    let filmDiv = `<article id="director${[i]}">`
+    filmDiv = filmDiv + '<img src="' + readPropertyFromFilm(dataToPrint[i], "posterDirector") + '">';
     filmDiv = filmDiv + '<h2>'
-    filmDiv = filmDiv + readPropertyFromFilm(dataToPrint[i], "director") +'<br>';
+    filmDiv = filmDiv + readPropertyFromFilm(dataToPrint[i], "director") + '<br>';
     filmDiv = filmDiv + '</h2>'
     filmDiv = filmDiv + '</article>'
     allFilms = filmDiv + allFilms
   }
   filmsDiv.innerHTML = allFilms;
 }
-function sortDataByMovieProducerAsc(){
+function sortDataByMovieProducerAsc() {
   let orderData = sortData(data.films, "producer", ORDER_ASCENDENTE);
 
   //limpiar el div contentPageTwo
@@ -136,7 +141,7 @@ function sortDataByMovieProducerAsc(){
   showFilmsByProducer(orderData);
 }
 
-function sortDataByMovieProducerDes(){
+function sortDataByMovieProducerDes() {
   let orderData = sortData(data.films, "producer", ORDER_DESCENDENTE);
 
   //limpiar el div contentPageTwo
@@ -148,14 +153,14 @@ function sortDataByMovieProducerDes(){
 }
 
 let buttonSorByMovieProducerAsc = document.getElementById("sortDataByMovieProducerAsc");
-buttonSorByMovieProducerAsc.addEventListener("click",sortDataByMovieProducerAsc)
+buttonSorByMovieProducerAsc.addEventListener("click", sortDataByMovieProducerAsc)
 
 let buttonSorByMovieProducerDes = document.getElementById("sortDataByMovieProducerDes");
-buttonSorByMovieProducerDes.addEventListener("click",sortDataByMovieProducerDes)
+buttonSorByMovieProducerDes.addEventListener("click", sortDataByMovieProducerDes)
 
 
 //para ordenar las peliculas por año
-function filterDataByYearAsc(){
+function filterDataByYearAsc() {
   const orderData = sortData(data.films, "release_date", ORDER_ASCENDENTE);
   //limpiar el div contentPageTwo
   const filmsDiv = document.getElementById("contentPageTwo");
@@ -164,7 +169,7 @@ function filterDataByYearAsc(){
   showFilmsByYear(orderData);
 }
 
-function filterDataByYearDes(){
+function filterDataByYearDes() {
   const orderData = sortData(data.films, "release_date", ORDER_DESCENDENTE);
   //limpiar el div contentPageTwo
   const filmsDiv = document.getElementById("contentPageTwo");
@@ -173,10 +178,10 @@ function filterDataByYearDes(){
   showFilmsByYear(orderData);
 }
 const buttonFilterByYearAsc = document.getElementById("filterDataByYearAsc");
-buttonFilterByYearAsc.addEventListener("click",filterDataByYearAsc)
+buttonFilterByYearAsc.addEventListener("click", filterDataByYearAsc)
 
 const buttonFilterByYearDes = document.getElementById("filterDataByYearDes");
-buttonFilterByYearDes.addEventListener("click",filterDataByYearDes)
+buttonFilterByYearDes.addEventListener("click", filterDataByYearDes)
 
 
 function showFilmsByProducer(dataToPrint) {
@@ -184,7 +189,7 @@ function showFilmsByProducer(dataToPrint) {
   let allFilms = '';
 
   for (let i = 0; i < dataToPrint.length; i++) {
-    let filmDiv = '<div>'
+    let filmDiv = `<div id="filmByProducer${[i]}">`
     filmDiv = filmDiv + '<img src="' + readPropertyFromFilm(dataToPrint[i], "poster") + '">';
     filmDiv = filmDiv + '<h3>'
     filmDiv = filmDiv + readPropertyFromFilm(dataToPrint[i], "title") + '<br>';
@@ -198,18 +203,17 @@ function showFilmsByProducer(dataToPrint) {
   filmsDiv.innerHTML = allFilms;
 
 }
-function showFilmsByYear(dataToPrint){
+function showFilmsByYear(dataToPrint) {
   const filmsDiv = document.getElementById("contentPageTwo");
-  let allFilms='';
-
-  for (let i=0; i< dataToPrint.length ; i++){
-    let filmDiv = '<div>'
-    filmDiv = filmDiv + '<img src="'+ readPropertyFromFilm(dataToPrint[i], "poster") + '">';
+  let allFilms = '';
+  for (let i = 0; i < dataToPrint.length; i++) {
+    let filmDiv = `<div id="film${[i]}">`
+    filmDiv = filmDiv + '<img src="' + readPropertyFromFilm(dataToPrint[i], "poster") + '">';
     filmDiv = filmDiv + '<h3>'
-    filmDiv = filmDiv + readPropertyFromFilm(dataToPrint[i], "title") +'<br>';
+    filmDiv = filmDiv + readPropertyFromFilm(dataToPrint[i], "title") + '<br>';
     filmDiv = filmDiv + '</h3>'
     filmDiv = filmDiv + '<h1>'
-    filmDiv = filmDiv + readPropertyFromFilm(dataToPrint[i], "release_date") +'<br>';
+    filmDiv = filmDiv + readPropertyFromFilm(dataToPrint[i], "release_date") + '<br>';
     filmDiv = filmDiv + '</h1>'
     filmDiv = filmDiv + '</div>'
     allFilms = filmDiv + allFilms
@@ -218,26 +222,73 @@ function showFilmsByYear(dataToPrint){
 }
 
 
-
-const  imgDiv = document.getElementById('buttonSearch')
-imgDiv.addEventListener('click',function (){
+/*funcionalidad del boton*/
+const imgDiv = document.getElementById('buttonSearch')
+imgDiv.addEventListener('click', function () {
   const textFilter = document.getElementById("seekerInput").value;
-  const filmsFiltered = filterData(arrayFilms,textFilter);
+  const filmsFiltered = filterData(arrayFilms, textFilter);
   showFilms(filmsFiltered);
 })
 /*que busque al dar enter*/
 const elem = document.getElementById('seekerInput');
-elem.addEventListener("keyup", function(e) {
+elem.addEventListener("keyup", function (e) {
   if (e.key === "Enter") {
     const textFilter = document.getElementById("seekerInput").value;
-    const filmsFiltered = filterData(arrayFilms,textFilter);
+    const filmsFiltered = filterData(arrayFilms, textFilter);
     showFilms(filmsFiltered);
   }
 });
+/*para que la busqueda se de mientras se escribe*/
+elem.addEventListener("keyup", function () {
+  const textFilter = document.getElementById("seekerInput").value;
+  const filmsFiltered = filterData(arrayFilms, textFilter);
+  showFilms(filmsFiltered);
+});
+/*datos en minuscula
+1ra letra a lower y 2da+ upper*/
+
+
 
 /*function computeStats (){
-<<<<<<< HEAD
-=======
-
->>>>>>> c5c4c631a0f6e936b6fb1c8c41097f7bda36f96a
 }*/
+
+
+/*mostrar historia 3*/
+function showPage3(filmsDisplay, arrayFilms) {
+  const filmsDiv = document.getElementById("contentPageTwo");
+  let allFilms = '';
+
+  for (let i = 0; i < filmsDisplay.length; i++) {
+    filmsDisplay[i].addEventListener('click', function filmInformation() {
+
+      for (let y = 0; y < arrayFilms.length; y++) {
+        if (i == y) {
+          const j = Math.abs(y-19)
+          let filmDiv = `<div style="display:block;text-align:justify;width:70%;">`
+          filmDiv = filmDiv + '<img style="height:40vh;width:16vw;" src="' + arrayFilms[j].poster + '"><br><br>';
+          filmDiv = filmDiv + '<h2>'
+          filmDiv = filmDiv + arrayFilms[j].title + '<br> <br>';
+          filmDiv = filmDiv + '</h2>'
+          filmDiv = filmDiv + '<h4>'
+          filmDiv = filmDiv + '<p>Director: ' + arrayFilms[j].director + '</p><br>';
+          filmDiv = filmDiv + '<p>Productor: ' + arrayFilms[j].producer + '</p><br>';
+          filmDiv = filmDiv + '</p>Release date: ' + arrayFilms[j].release_date + '</p><br>';
+          filmDiv = filmDiv + '<p>Description: ' + arrayFilms[j].description + '</p>';
+          filmDiv = filmDiv + '</h4>'
+          filmDiv = filmDiv + '</div>'
+          allFilms = filmDiv + allFilms
+        }
+        filmsDiv.innerHTML = allFilms;
+        arrowBack.style.display= "";
+      }
+    });
+  }
+}
+
+const arrowBack = document.getElementById("arrowBack");
+arrowBack.addEventListener("click", ()=>{
+  const filmsDiv = document.getElementById("contentPageTwo");
+  filmsDiv.innerHTML = "";
+  arrowBack.style.display= "none";
+  showPage2()
+})
