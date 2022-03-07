b1dde3fe3e2e4e9bdc64a36359453f37e61b6726
Shirley B. Aguilar <shirley.aguilar.agui@gmail.com>
2022-03-07 18:47:15 -0500

    cambiando condicional if  a operador ternario en funcion sort data, agregando test unitarios  filterData y sort data

--- src/data.js
+++ src/data.js
@@ -8,26 +8,21 @@ export const filterData = (data,condition) => {
 
 // Defines lo que recibe, lo que hace y lo que devuelve
 // recibe : recibe la data que es un objeto y condition que es una propiedad del objeto
-//  hace:  está obteniendo la propiedad del objeto 
-//devuelve: el elemento del objeto.propiedad ejemplo data.title : "HOLIS"
+// hace:  está obteniendo la propiedad del objeto 
+// devuelve: el elemento del objeto.propiedad ejemplo data.title : "HOLIS"
 
 // Defines lo que debe recibir, lo que debe hacer, y lo que debe devolver
-//recibir : debe recibir un arreglo con una condicion
-//hacer: debe evaluar al arreglo y traer los elementos que cumplan tal condicion
-//devolver: los elementos que cumplan la condicion 
+// recibir : debe  recibir un arreglo con una condicion
+// hacer: debe evaluar al arreglo y traer los elementos que cumplan tal condicion
+// devolver: los elementos que cumplan la condicion 
 
 export const sortData = (data, sortBy, sortOrder) => {
   // return '';  data= datos sin orden  sortBy= con respecto a cual de los datos se va a ordenar  sortorder= forma de orden(ascendete, descendente, alfabeticamente)
   let stringdata = JSON.stringify(data);
   let stringdataParse = JSON.parse(stringdata);
-  //ordenando objetos por title
-  if (sortOrder === ORDER_ASCENDENTE) {
-    return stringdataParse.sort(getSortOrderAsc(sortBy));
-  } else if (sortOrder === ORDER_DESCENDENTE){
-    return stringdataParse.sort(getSortOrderDesc(sortBy));
-  } else {
-    console.log("error de configuración en la función sortData, sortOrder :" + sortOrder );
-  }
+  // ordenando objetos por title
+
+  return (sortOrder === ORDER_ASCENDENTE) ?  stringdataParse.sort(getSortOrderAsc(sortBy)) :stringdataParse.sort(getSortOrderDesc(sortBy));  
 };
 
 export const ORDER_ASCENDENTE = "asc";
@@ -57,13 +52,13 @@ function getSortOrderDesc(prop) {
 
 export const computeStats = (data, specieToCount) => {
     // return 'OMG'; data=datos sin orden   hace calculo en base a la data
-    var contador = 0; //variable que indica cuantas veces tenemos tipoEspecie dentro de algun item de people
+    var contador = 0; // variable que indica cuantas veces tenemos tipoEspecie dentro de algun item de people
 
-    //dentro de cada film
+    // dentro de cada film
     for(var i = 0; i < data.length; i ++ ) {
       
       var auxPeople = data[i].people;
-      //dentro de cada people
+      // dentro de cada people
       for(var j = 0; j < data[i].people.length; j++) {
         if(auxPeople[j].specie === specieToCount) {
           contador++;
