bdc39f5dd08a318859a5312c12061ba7191501e7
yaisa03 <yaisamichellea@gmail.com>
2022-03-07 22:16:06 -0500

    carrusel y formato de main.js

--- src/main.js
+++ src/main.js
@@ -2,23 +2,8 @@ import data from './data/ghibli/ghibli.js';
 import { filterData, sortData, computeStats, ORDER_ASCENDENTE, ORDER_DESCENDENTE } from './data.js';
 
 const arrayFilms = data.films;
-/*flecha para volver a historia 2*/
+// flecha para volver a historia 2
 const arrowBack = document.getElementById("arrowBack");
-
-
-
-//CARRUSEL
-const accesPage1 = document.getElementById('buttonContinuar');
-accesPage1.addEventListener("click",hideCarrusel);
-function  hideCarrusel (){
-  document.getElementById("carrusel-container").style.display = "none";
-  showPageOne();
-}
-function showPageOne(){
-  document.getElementById("pageOne").style.display = "block";
-}
-
-
 
 //PRIMERA HISTORIA
 const buttonAccess = document.getElementById("buttonAccess");
@@ -35,22 +20,22 @@ function showPage2() {
   document.getElementById("pageOne").style.display = "none";
   document.getElementById("pageTwo").style.display = "block";
   showFilms(arrayFilms);
-  /*mostrar historia 3*/
+  // mostrar historia 3
   const filmsDisplay = document.querySelectorAll(`div[id^="film"]`);
   showPage3(filmsDisplay, arrayFilms);
-  /*volver a historia 2*/
+  // volver a historia 2
   arrowBack.addEventListener("click", () => {
     const filmsDiv = document.getElementById("contentPageTwo");
     filmsDiv.innerHTML = "";
     arrowBack.style.display = "none";
-    /*historia 2*/
+    // historia 2
     showFilms(arrayFilms);
     /*mostrar historia 3*/
     const filmsDisplay = document.querySelectorAll(`div[id^="film"]`);
     showPage3(filmsDisplay, arrayFilms);
   });
 }
-//Mostrar peliculas: Sin orden
+// Mostrar peliculas: Sin orden
 function showFilms(dataToPrint) {
   const filmsDiv = document.getElementById("contentPageTwo");
   let filmDiv = '';
@@ -69,22 +54,22 @@ function showFilms(dataToPrint) {
   return filmsDiv;
 }
 
-//Peliculas por TITULO
+// Peliculas por TITULO
 let buttonFilterByMovieTitleAsc = document.getElementById("filterDataByMovieTitleAsc");
-buttonFilterByMovieTitleAsc.addEventListener("click", filterDataByMovieTitleAsc); //llama a las films ordenados A-Z
+buttonFilterByMovieTitleAsc.addEventListener("click", filterDataByMovieTitleAsc); // llama a las films ordenados A-Z
 
 function filterDataByMovieTitleAsc() {
   let orderData = sortData(data.films, "title", ORDER_ASCENDENTE);
-  //limpiar el div contentPageTwo
+  // limpiar el div contentPageTwo
   const filmsDiv = document.getElementById("contentPageTwo");
   filmsDiv.innerHTML = "";
   hideMenuNav();
-  //pintar los objetos ya ordenados
+  // pintar los objetos ya ordenados
   showFilms(orderData);
-  /*mostrar historia 3*/
+  // mostrar historia 3
   const filmsDisplay = document.querySelectorAll(`div[id^="film"]`);
   showPage3(filmsDisplay, orderData)
-  //flecha para vlver a peliculas ascendente
+  // flecha para vlver a peliculas ascendente
   arrowBack.addEventListener("click", () => {
     const filmsDiv = document.getElementById("contentPageTwo");
     filmsDiv.innerHTML = "";
@@ -94,7 +79,7 @@ function filterDataByMovieTitleAsc() {
 }
 
 let buttonFilterByMovieTitleDesc = document.getElementById("filterDataByMovieTitleDesc");
-buttonFilterByMovieTitleDesc.addEventListener("click", filterDataByMovieTitleDesc); //llama a las films ordenados Z-A
+buttonFilterByMovieTitleDesc.addEventListener("click", filterDataByMovieTitleDesc); // llama a las films ordenados Z-A
 
 function filterDataByMovieTitleDesc() {
   let orderData = sortData(data.films, "title", ORDER_DESCENDENTE);
@@ -102,10 +87,10 @@ function filterDataByMovieTitleDesc() {
   filmsDiv.innerHTML = "";
   hideMenuNav();
   showFilms(orderData);
-  /*mostrar historia 3*/
+  // mostrar historia 3
   const filmsDisplay = document.querySelectorAll(`div[id^="film"]`);
   showPage3(filmsDisplay, orderData)
-  //flecha para peliculas descendente
+  // flecha para peliculas descendente
   arrowBack.addEventListener("click", () => {
     const filmsDiv = document.getElementById("contentPageTwo");
     filmsDiv.innerHTML = "";
@@ -114,8 +99,8 @@ function filterDataByMovieTitleDesc() {
   });
 }
 
-//EVENTOS PARA DIRECTORES
-//----------evento ascendente-----------
+// EVENTOS PARA DIRECTORES
+// evento ascendente
 let buttonFilterByMovieDirectorAsc = document.getElementById("filterDataByMovieDirectorAsc");
 buttonFilterByMovieDirectorAsc.addEventListener("click", filterDataByMovieDirectorAsc);
 
@@ -128,14 +113,14 @@ function filterDataByMovieDirectorAsc() {
       onlyDirectors.push(p);
     }
   });
-  //limpiar
+  // limpiar
   const filmsDiv = document.getElementById("contentPageTwo");
   filmsDiv.innerHTML = "";
-  //mostrar directores
+  // mostrar directores
   hideMenuNav();
   showFilmsDirector(onlyDirectors);
 }
-//----------evento descendente-----------
+// evento descendente
 let buttonFilterByMovieDirectorDesc = document.getElementById("filterDataByMovieDirectorDesc");
 buttonFilterByMovieDirectorDesc.addEventListener("click", filterDataByMovieDirectorDesc);
 
@@ -148,15 +133,15 @@ function filterDataByMovieDirectorDesc() {
       onlyDirectors.push(p);
     }
   });
-  //limpiar
+  // limpiar
   const filmsDiv = document.getElementById("contentPageTwo");
   filmsDiv.innerHTML = "";
-  //mostrar directores
+  // mostrar directores
   hideMenuNav();
   showFilmsDirector(onlyDirectors);
 }
 
-//MOSTRAR DIRECTORES
+// MOSTRAR DIRECTORES
 function showFilmsDirector(dataToPrint) {
   const filmsDiv = document.getElementById("contentPageTwo");
   let allFilms = '';
@@ -171,7 +156,7 @@ function showFilmsDirector(dataToPrint) {
   filmsDiv.innerHTML = allFilms;
 }
 
-//Ordena las peliculas por PRODUCTORES
+// Ordena las peliculas por PRODUCTORES
 let buttonSorByMovieProducerAsc = document.getElementById("sortDataByMovieProducerAsc");
 buttonSorByMovieProducerAsc.addEventListener("click", sortDataByMovieProducerAsc); // ordena A-Z
 
@@ -184,10 +169,10 @@ function sortDataByMovieProducerAsc() {
       onlyProducers.push(p);
     }
   });
-  //limpiar el div contentPageTwo
+  // limpiar el div contentPageTwo
   const filmsDiv = document.getElementById("contentPageTwo");
   filmsDiv.innerHTML = "";
-  //pintar los objetos ya ordenados
+  // pintar los objetos ya ordenados
   hideMenuNav();
   showFilmsByProducer(onlyProducers);
 }
@@ -204,15 +189,15 @@ function sortDataByMovieProducerDes() {
       onlyProducersDesc.push(p);
     }
   });
-  //limpiar el div contentPageTwo
+  // limpiar el div contentPageTwo
   const filmsDiv = document.getElementById("contentPageTwo");
   filmsDiv.innerHTML = "";
-  //pintar los objetos ya ordenados
+  // pintar los objetos ya ordenados
   hideMenuNav();
   showFilmsByProducer(onlyProducersDesc);
 }
 
-//Muestra los productores
+// Muestra los productores
 function showFilmsByProducer(dataToPrint) {
   const producersDiv = document.getElementById("contentPageTwo");
   let producerDiv = "";
@@ -226,22 +211,22 @@ function showFilmsByProducer(dataToPrint) {
   return producersDiv;
 }
 
-//para ordenar las peliculas por año
+// para ordenar las peliculas por año
 const buttonFilterByYearAsc = document.getElementById("filterDataByYearAsc");
 buttonFilterByYearAsc.addEventListener("click", filterDataByYearAsc);
 
 function filterDataByYearAsc() {
   const orderData = sortData(data.films, "release_date", ORDER_ASCENDENTE);
-  //limpiar el div contentPageTwo
+  // limpiar el div contentPageTwo
   const filmsDiv = document.getElementById("contentPageTwo");
   filmsDiv.innerHTML = "";
-  //pintar los objetos ya ordenados
+  // pintar los objetos ya ordenados
   hideMenuNav();
   showFilmsByYear(orderData);
-  /*Historia 3*/
+  // Historia 3
   const filmsDisplay = document.querySelectorAll(`div[id^="film"]`);
   showPage3(filmsDisplay, orderData)
-  //flecha para peliculas ascendente
+  // flecha para peliculas ascendente
   arrowBack.addEventListener("click", () => {
     const filmsDiv = document.getElementById("contentPageTwo");
     filmsDiv.innerHTML = "";
@@ -255,16 +240,16 @@ buttonFilterByYearDes.addEventListener("click", filterDataByYearDes);
 
 function filterDataByYearDes() {
   const orderData = sortData(data.films, "release_date", ORDER_DESCENDENTE);
-  //limpiar el div contentPageTwo
+  // limpiar el div contentPageTwo
   const filmsDiv = document.getElementById("contentPageTwo");
   filmsDiv.innerHTML = "";
-  //pintar los objetos ya ordenados
+  // pintar los objetos ya ordenados
   hideMenuNav();
   showFilmsByYear(orderData);
-  /*Historia 3*/
+  // Historia 3
   const filmsDisplay = document.querySelectorAll(`div[id^="film"]`);
   showPage3(filmsDisplay, orderData)
-  //flecha para peliculas ascendente
+  // flecha para peliculas ascendente
   arrowBack.addEventListener("click", () => {
     const filmsDiv = document.getElementById("contentPageTwo");
     filmsDiv.innerHTML = "";
@@ -288,18 +273,18 @@ function showFilmsByYear(dataToPrint) {
   filmsDiv.innerHTML = filmDiv;
   return filmsDiv;
 }
-//BUSCADOR
-/*que busque al dar enter*/
+// BUSCADOR
+// que busque al dar enter
 const elem = document.getElementById('seekerInput');
 elem.addEventListener("keyup", function (e) {
   if (e.key === "Enter") {
     const textFilter = document.getElementById("seekerInput").value;
     const filmsFiltered = filterData(arrayFilms, textFilter);
     showFilms(filmsFiltered);
-    /*mostrar historia 3*/
+    // mostrar historia 3 
     const filmsDisplay = document.querySelectorAll(`div[id^="film"]`);
     showPage3(filmsDisplay, filmsFiltered);
-    /*volver a historia 2*/
+    // volver a historia 2
     arrowBack.addEventListener("click", () => {
       const filmsDiv = document.getElementById("contentPageTwo");
       filmsDiv.innerHTML = "";
@@ -310,15 +295,15 @@ elem.addEventListener("keyup", function (e) {
     });
   }
 });
-/*para que la busqueda se de mientras se escribe*/
+// para que la busqueda se de mientras se escribe
 elem.addEventListener("keyup", function () {
   const textFilter = document.getElementById("seekerInput").value;
   const filmsFiltered = filterData(arrayFilms, textFilter);
   showFilms(filmsFiltered);
-  /*mostrar historia 3*/
+  // mostrar historia 3
   const filmsDisplay = document.querySelectorAll(`div[id^="film"]`);
   showPage3(filmsDisplay, filmsFiltered);
-  /*volver a historia 2*/
+  // volver a historia 2
   arrowBack.addEventListener("click", () => {
     const filmsDiv = document.getElementById("contentPageTwo");
     filmsDiv.innerHTML = "";
@@ -329,7 +314,7 @@ elem.addEventListener("keyup", function () {
   });
 });
 
-//ocultar menu de navegador
+// ocultar menu de navegador
 let clickLabelCheck = document.getElementById("labelCheck");
 clickLabelCheck.addEventListener("click", hideMenuNav);
 
@@ -346,7 +331,7 @@ function showMenuNav() {
   navegadorMenu.classList.remove('hideMenu')
 }
 
-/*mostrar historia 3*/
+// mostrar historia 3
 function showPage3(filmsDisplay, array) {
   const filmsDiv = document.getElementById("contentPageTwo");
   let filmInfo = '';
@@ -375,12 +360,11 @@ function showPage3(filmsDisplay, array) {
   }
 }
 
-
 function computeTotalNumberOfSpecie(specie) {
-  //limpiar el div contentPageTwo
+  // limpiar el div contentPageTwo
   const filmsDiv = document.getElementById("contentPageTwo");
   filmsDiv.innerHTML = "";
-  //contador
+  // contador
   let numberOfSpecie = computeStats(arrayFilms, specie);
   hideMenuNav();
   showFilmsDataPeople(numberOfSpecie);
@@ -414,7 +398,7 @@ function computeTotalNumberOfWolf() {
 
 }
 
-//mostrando conteo-------
+// mostrando conteo
 function showFilmsDataPeople(dataToPrint) {
   let allFilms = "";
   const FILMS_DIV = document.getElementById("contentPageTwo");
@@ -425,8 +409,8 @@ function showFilmsDataPeople(dataToPrint) {
   allFilms = filmDiv;
   FILMS_DIV.innerHTML = allFilms;
 }
-// evento de compute stats
 
+// evento de compute stats
 const buttonFilterDataByHuman = document.getElementById("filterDataByHuman");
 buttonFilterDataByHuman.addEventListener("click", computeTotalNumberOfHuman);
 
@@ -451,54 +435,44 @@ buttonFilterDataBySpirit.addEventListener("click", computeTotalNumberOfSpirit);
 const buttonFilterDataByWolf = document.getElementById("filterDataByWolf");
 buttonFilterDataByWolf.addEventListener("click", computeTotalNumberOfWolf);
 
-
-
 // carrusel
 let indexUl = document.querySelector(".imgdiv ul");
-//etiqueta li
+// etiqueta li
 let  indexLi=indexUl.querySelectorAll("li");
 let  lengthLi =indexLi.length;
 //el div más grande
 let pop = document.querySelector(".imgdiv");
 let lfspan=pop.querySelector('.lf');
 let rfspan=pop.querySelector('.rf');
-//Establecer los botones izquierdo y derecho al centro
-// lfspan.style.top=(parseInt(pop.offsetHeight)-60)/2+'px';
-// rfspan.style.top=(parseInt(pop.offsetHeight)-60)/2+'px';
-//Inicializar para obtener el ancho del marco de visualización
-let content_width=parseInt(pop.offsetWidth);
-//
+
 const TIME=2000;
 let timer;
-//
-//Configura el acumulador global para determinar qué imagen mover ****************
 let num=0;
-//Sigue el número de imágenes, establece dinámicamente el número de puntos
+
 let newsbigdiv=document.createElement('div');
 newsbigdiv.className='navigators';
 pop.appendChild(newsbigdiv);
-//Crea subdivs en un bucle, da a cada subdiv un estilo de clase y agrega un índice
+// Crea subdivs en un bucle, da a cada subdiv un estilo de clase y agrega un índice
 for(var i=0;i<lengthLi;i++){
   let  newsdiv=document.createElement('div');
   newsbigdiv.appendChild(newsdiv);
   newsdiv.className="newsdiv";
-  newsdiv.setAttribute('index',i);//Establecer un valor de índice para cada subdiv.
+  newsdiv.setAttribute('index',i);// Establecer un valor de índice para cada subdiv.
 }
-//Obtenga el número de divisiones de puntos, desplace hacia la izquierda y muéstrelo en el centro
+// Obtenga el número de divisiones de puntos, desplace hacia la izquierda y muéstrelo en el centro
 let divsv = newsbigdiv.querySelectorAll('div');
-//El primero está resaltado por defecto
+// El primero está resaltado por defecto
 divsv[0].style.opacity=1;
 //
-//
 function change(){
-  //Establece toda la transparencia de estilo de punto en 0.3
+  // Establece toda la transparencia de estilo de punto en 0.3
   for(let j=0;j<lengthLi;j++){
     divsv[j].style.opacity=0.3;
   }
-  //Cambia el resaltado de puntos a la imagen con el índice actual como num
+  // Cambia el resaltado de puntos a la imagen con el índice actual como num
   divsv[num].style.opacity=1;
   indexUl.style.left=-num*100+'%';
-  //indexUl.classList.add('slide-1')
+  // indexUl.classList.add('slide-1')
 }
 //
 for(let i=0;i<lengthLi;i++){
@@ -523,12 +497,11 @@ rfspan.onclick=function(){
 }
 //
 timer=setInterval(rfspan.onclick,TIME);
-//
-//Borra el temporizador cuando el mouse esté sobre la imagen
+// Borra el temporizador cuando el mouse esté sobre la imagen
 pop.onmouseover=function(){
   clearInterval(timer);
 }
-//Inicia el temporizador cuando el mouse sale del temporizador
+// Inicia el temporizador cuando el mouse sale del temporizador
 pop.onmouseout=function(){
   timer=setInterval(rfspan.onclick,TIME);
 }
